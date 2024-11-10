d010993ed790dec08a5e4d32b210b96a1ddb5494
osamanisar05 <164087967+osamanisar05@users.noreply.github.com>
2024-11-10 20:21:32 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-import Header from "./component/header";
-import Footer  from "./component/Footer";
 import Responsive from "./responsive/page";
-
-
-
-
-
 export default function Home() {
   return (
     <div>
